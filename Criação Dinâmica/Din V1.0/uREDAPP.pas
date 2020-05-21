e730dfd6da0941d82b751c438edff8e206ed23ea
shollzz <lucashazzi@gmail.com>
2020-05-21 16:21:13 -0300

    Versão 1.0 da Biblioteca Dinâmica

---
+++ Criação Dinâmica/Din V1.0/uREDAPP.pas
@@ -0,0 +1,12 @@
+{ REGISTRADOR DE ESTADO DO APP - REDAPP }
+{ !!! AINDA EM FASE DE IMPLEMENTAÇÃO !!! }
+unit uREDAPP;
+
+interface
+
+type
+  TEstado = (Alteracao, Inclusao, Leitura);
+
+implementation
+
+end.
